afd6c03c64b0256918cbbd0401d93ccbd8c3e77f
Fnu Pawan Kumar <pawan.3940@gmail.com>
2023-04-12 13:30:03 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+
+
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"}
+]
+const superbowlWin=(record) => {
+  const winYear= record.find(Element=> Element.result==='W')
+  debugger
+  if(winYear===undefined){
+      return undefined
+      
+  }
+  else if(winYear.result==='W') {
+      return winYear.year
+  }
+}
+  
